4c511ab3e59a9e9bd8f3fca6dc91c7eb26e7499d
Adnan Rahin <adnan.rahin13@gmail.com>
2019-08-20 01:19:33 -0400

    TensorFlow-Pandas-Libarary

--- TensorFlow-Pandas-Libarary/dataframepandas.py
+++ TensorFlow-Pandas-Libarary/dataframepandas.py
@@ -7,5 +7,13 @@ pd.set_option('display.width', 1000)
 
 california_housing_dataframe = pd.read_csv(
     "https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe.head()
 
 print(california_housing_dataframe)
+
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469, 1015785, 485199])
+
+data = pd.DataFrame({ 'City name': city_names, 'Population': population })
+california_housing_dataframe.hist('housing_median_age')
+print(data)
